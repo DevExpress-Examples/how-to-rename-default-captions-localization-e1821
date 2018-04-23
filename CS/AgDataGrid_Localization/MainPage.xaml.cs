ed6be165d987ed9f8b385d6e9d87537a5e3c801a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgDataGrid_Localization/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using DevExpress.AgDataGrid.Localization;
+
+namespace AgDataGrid_Localization {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            AgDataGridLocalizer.Active = new MyCustomLocalizer();
+            grid.DataSource = ProductList.GetData();
+        }
+    }
+
+    public class MyCustomLocalizer : AgDataGridLocalizer {
+        // Replaces the default string displayed within the empty Column Chooser.
+        public override string GetLocalizedString(AgDataGridStringId id) {
+            if (id == AgDataGridStringId.ColumnChooserEmptyText)
+                return "To hide a column, drop its header here";
+            return base.GetLocalizedString(id);
+        }
+    }
+}
---
+++ VB/AgDataGrid_Localization/MainPage.xaml.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+Imports DevExpress.AgDataGrid.Localization
+
+Namespace AgDataGrid_Localization
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			AgDataGridLocalizer.Active = New MyCustomLocalizer()
+			grid.DataSource = ProductList.GetData()
+		End Sub
+	End Class
+
+	Public Class MyCustomLocalizer
+		Inherits AgDataGridLocalizer
+		' Replaces the default string displayed within the empty Column Chooser.
+		Public Overrides Function GetLocalizedString(ByVal id As AgDataGridStringId) As String
+			If id = AgDataGridStringId.ColumnChooserEmptyText Then
+				Return "To hide a column, drop its header here"
+			End If
+			Return MyBase.GetLocalizedString(id)
+		End Function
+	End Class
+End Namespace
